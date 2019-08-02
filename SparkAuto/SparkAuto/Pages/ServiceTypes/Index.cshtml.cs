436c5646c212922d2ba0a44d2ccd922ef8ab1a8e
Miguel Aguila <mlaaguilamapua@gmail.com>
2019-08-01 23:41:49 -0700

    Implemented CRUD for service types & Initial Setup w/ Identity

---
+++ SparkAuto/SparkAuto/Pages/ServiceTypes/Index.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using SparkAuto.Data;
+using SparkAuto.Model;
+
+namespace SparkAuto.Pages.ServiceTypes
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+
+        //Dependency Injection
+        public IndexModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IList<ServiceType> ServiceType { get; set; }
+
+        //All handlers need to start with 'On'
+        public async Task<IActionResult> OnGet()
+        {
+            ServiceType = await _db.ServiceType.ToListAsync();
+            return Page();
+        }
+    }
+}
